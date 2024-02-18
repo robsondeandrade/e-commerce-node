ac87caf87537fe9c8155d11f686fec8fe3853993
Robson Andrade <robson.andrade19@gmail.com>
2024-02-18 15:21:05 -0300

    feat: add favoriteId field to Favorite model and schema validation

--- src/validations/favorite/AddFavoriteSchema.ts
+++ src/validations/favorite/AddFavoriteSchema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 export const AddFavoriteSchema = z.object({
     title: z.string(),
     thumbnail: z.string(),
-    quantity: z.number(),
+    favoriteId: z.string(),
     available_quantity: z.number(),
     price: z.number(),
 })
